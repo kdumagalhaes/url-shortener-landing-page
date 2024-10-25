51d6a3b327782f9c87c10dca4bd24db0c5c59d00
carlospereira <ce.magalhaespereira@gmail.com>
2024-10-25 15:05:38 +0100

    Add warning message

--- app/components/UrlInput/UrlInput.tsx
+++ app/components/UrlInput/UrlInput.tsx
@@ -1,14 +1,32 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./UrlInput.module.scss";
 export function UrlInput() {
+  const [url, setUrl] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  const validateUrl = () => {
+    if (!url) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  };
+
   return (
     <div className={styles.urlInput}>
-      <input
-        placeholder="Shorten a link here..."
-        autoFocus
-        className={styles.input}
-        type="text"
-      />
-      <button>Shorten It!</button>
+      <div className={styles.inputContainer}>
+        <input
+          placeholder="Shorten a link here..."
+          autoFocus
+          className={`${styles.input} ${isEmpty && styles["input--warning"]}`}
+          type="text"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        {isEmpty && <p className={styles.warningMessage}>Please add a link</p>}
+      </div>
+      <button onClick={validateUrl}>Shorten It!</button>
     </div>
   );
 }
